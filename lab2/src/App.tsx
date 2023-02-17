e60a84a9f649dc6717acf337ada43544472919cd
baltean2000 <baltean2000@gmail.com>
2023-02-17 16:23:42 +0200

    Lab2

---
+++ lab2/src/App.tsx
@@ -0,0 +1,16 @@
+import {observer} from 'mobx-react-lite';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+  <div className ="App">
+  <h1> Datele mele personale:</h1>
+  <p>Nume/Prenume: Baltean Sergiu</p>
+  <p>Grupa: CR-212</p>
+  </div>
+  );
+}
+
+export default App;
